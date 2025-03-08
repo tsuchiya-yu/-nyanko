3b0a5c10ee4208b7152d7ead5a87c1a078549ec7
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2025-03-08 17:13:23 +0900

    chore: リンター追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -9,6 +11,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    typography,
   ],
 };

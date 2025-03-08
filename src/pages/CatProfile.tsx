f383fcb0d0670b4be2b5f5f8638a018b9615da51
tsuchiya-yu <37299387+tsuchiya-yu@users.noreply.github.com>
2025-03-08 15:44:36 +0000

    style: apply lint and format fixes

--- src/pages/CatProfile.tsx
+++ src/pages/CatProfile.tsx
@@ -317,7 +317,16 @@ export default function CatProfile() {
                   rel="noopener noreferrer"
                   className="inline-flex items-center text-gray-500 hover:text-gray-600"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-8 w-8 mr-2"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
                     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                     <polyline points="9 22 9 12 15 12 15 22"></polyline>
                   </svg>

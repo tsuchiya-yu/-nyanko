de6b1bac0badc866b8fc9c457f1a84097aa29091
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2025-03-12 20:23:05 +0900

    push漏れ

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { next } from "@vercel/edge";
+
+export const config = {
+  matcher: "/(.*)",  // すべてのルートに適用
+};
+
+export default function middleware(request: Request) {
+  const authorizationHeader = request.headers.get("authorization");
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(" ")[1];
+    const [user, password] = atob(basicAuth).toString().split(":");
+
+    if (user === process.env.BASIC_AUTH_USER && password === process.env.BASIC_AUTH_PASSWORD) {
+      return next();
+    }
+  }
+
+  return new Response("Basic認証が必要です", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="猫アプリ専用エリア"',
+    },
+  });
+} 
